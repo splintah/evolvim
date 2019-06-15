5a98dca9b12876f7770cf66706e9e352cc389be6
Splinter Suidman <splinter@mannenopdemaan.nl>
2019-06-15 19:19:17 +0200

    Start wasm experiment.

---
+++ evolvim-web/src/lib.rs
@@ -0,0 +1,57 @@
+use wasm_bindgen::prelude::*;
+use lib_evolvim as evolvim;
+use evolvim::*;
+
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[wasm_bindgen]
+pub struct Universe {
+    board: Board,
+}
+
+#[wasm_bindgen]
+impl Universe {
+    pub fn new() -> Self {
+        log("New universe!");
+        Universe { board: Board::default() }
+    }
+
+    pub fn width(&self) -> usize {
+        self.board.get_board_width()
+    }
+
+    pub fn height(&self) -> usize {
+        self.board.get_board_height()
+    }
+
+    pub fn update(&mut self) {
+        self.board.update(10.0);
+    }
+
+    pub fn season(&self) -> String {
+        self.board.get_season()
+    }
+
+    pub fn time(&self) -> f64 {
+        self.board.get_time()
+    }
+
+    pub fn count_creatures(&self) -> usize {
+        self.board.creatures.len()
+    }
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    log(&format!("Hello, {}!", name));
+}
---
+++ evolvim-web/www/index.js
@@ -0,0 +1,19 @@
+import { Universe, greet } from "evolvim-web";
+
+const info = document.getElementById("evolvim-info");
+const universe = Universe.new();
+console.log(universe.width(), universe.height());
+
+const renderLoop = () => {
+    universe.update();
+    info.textContent =
+`time: ${universe.time()}
+season: ${universe.season()}
+width: ${universe.width()}
+height: ${universe.height()}
+creatures: ${universe.count_creatures()}
+`;
+    requestAnimationFrame(renderLoop);
+}
+
+renderLoop();
